f488edf4bff33d528f8361a8d4b58d12e2bae0c2
elizamthomas1994 <78053561+elizamthomas1994@users.noreply.github.com>
2021-04-11 12:51:24 +0100

    Implemented Axios and integrated Dictionary API into project
    While the API has been successfully integrated, I haven't yet made necessary adjustments so that the results are displayed on the page for the user

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response);
+    }
     
     function search(event) {
         event.preventDefault();
         alert(`Searching '${keyword}'...`);
+
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`
+        axios.get(apiUrl).then(handleResponse);
     }
+
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
