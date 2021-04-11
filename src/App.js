9d0fbc0e18b84556b28c268d4d05e6364a60c33c
elizamthomas1994 <78053561+elizamthomas1994@users.noreply.github.com>
2021-04-11 11:56:21 +0100

    Installed React in VS Code and installed Bootstrap into the public page of the project
    This is just setting things up to get started in coding the project!
    
    Deleted all the elements linked to the base React page, and just put in a quick filler button to insure Bootstrap had been properly implemented in the project,

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href="/" className="btn btn-primary">Hello</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
