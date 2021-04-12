7796e6bda48558861475e33aa8ef4ae8146bda7f
elizamthomas1994 <78053561+elizamthomas1994@users.noreply.github.com>
2021-04-12 19:01:02 +0100

    Created Results and Meaning components to display the dictionary definitions of input words
    The app now retrieves the data from the Dictionary API to display the definitions of any user-inputted words and displays them in an easy way. However, I still have yet to apply any CSS to this project. This will come later.

---
+++ src/Results.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Meaning from "./Meaning"
+
+export default function Results(props) {
+    console.log(props.results);
+    if (props.results) {
+        return(
+            <div className="Results">
+                <h1>{props.results.word}</h1>
+                {props.results.meanings.map(function(meaning, index) {
+                    return (
+                        <div key={index}>
+                            <Meaning meaning={meaning} />
+                        </div>
+                    )
+                })}
+            </div>
+        )
+        }
+    else {
+    return null;
+    }
+}
--- src/dictionary.js
+++ src/dictionary.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response);
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
     }
     
     function search(event) {
@@ -24,6 +27,7 @@ export default function Dictionary() {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange}/>
             </form>
+            <Results results={results} />
         </div>
     );
 }
