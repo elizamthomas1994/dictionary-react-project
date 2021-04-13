feddd16254731e001d430bbe8bd74d8d8db1eef3
elizamthomas1994 <78053561+elizamthomas1994@users.noreply.github.com>
2021-04-13 23:18:21 +0100

    Attempted to fix a bug which is preventing the deployment of this app through Netlify
    Opened up the console through inspect element to see there was a security risk in regards to a link in the Phonetic component, and have since made necessary changes to correct this issue.

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
     return(
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
                 Listen
             </a>
             <span className="pronounciation">{props.phonetic.text}</span>
