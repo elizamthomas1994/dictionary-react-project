c8cce8200e73109e9914f47e7e8a7892e99ffaaf
elizamthomas1994 <78053561+elizamthomas1994@users.noreply.github.com>
2021-04-13 12:40:22 +0100

    Added Phonetic component
    Now when users input words into the dictionary, the app will display how the word should be pronounced and will link to an audio file so that users can hear how the given word is spoken

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meaning from "./Meaning"
 
 export default function Results(props) {
@@ -7,6 +8,13 @@ export default function Results(props) {
         return(
             <div className="Results">
                 <h1>{props.results.word}</h1>
+                {props.results.phonetics.map(function(phonetic,index) {
+                    return(
+                        <div key={index}>
+                            <Phonetic phonetic={phonetic} />
+                        </div>
+                    )
+                })}
                 {props.results.meanings.map(function(meaning, index) {
                     return (
                         <div key={index}>
