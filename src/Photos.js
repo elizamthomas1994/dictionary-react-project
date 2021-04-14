cc4e00da97fead4ea916b9870db1b4e3b1e7e214
elizamthomas1994 <78053561+elizamthomas1994@users.noreply.github.com>
2021-04-14 17:56:49 +0100

    Corrected a bug preventing the ability to successfully deploy the app through Netlify

--- src/Photos.js
+++ src/Photos.js
@@ -10,8 +10,8 @@ export default function Photos(props) {
                     {props.photos.map(function(photo, index) {
                         return (
                             <div className="col-4" key={index}>
-                                <a href={photo.src.original} target="_blank" rel="noreferrer" alt="Preview image of your word!">
-                                    <img src={photo.src.landscape} className="img-fluid"/>
+                                <a href={photo.src.original} target="_blank" rel="noreferrer" >
+                                    <img src={photo.src.landscape} className="img-fluid" alt="Visual display of your word!"/>
                                 </a>
                             </div>
                         )
